1ee865d28a5906a8cb60b1ec14b55dfddacff08e
Jaqueline Kirino <bfatskih@gmail.com>
2022-02-21 14:36:52 -0300

    pnpm

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -146,7 +146,7 @@ pnpm_node_modules() {
 
   echo "Installing node modules (pnpm-lock.yaml)"
   cd "$build_dir" || return
-  monitor "pnpm-install" pnpm install -D
+  monitor "pnpm-install" pnpm install -D && pnpm install
 }
 
 yarn_2_install() {
